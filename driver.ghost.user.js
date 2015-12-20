d4f7e496d466b88076aab5380e34655e81a7a4b8
Tsung Wu <tsung.bz@gmail.com>
2015-12-20 15:37:34 +0800

    init

---
+++ driver.ghost.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        driver ghost
+// @description automatically commit driver training
+// @include     http://www.350500.prcjx.net/admin/std/training/*
+// @require     http://code.jquery.com/jquery-2.1.1.min.js
+// @version     3
+// ==/UserScript==
+$(function () {
+    var url = location.href;
+
+    function commit() {  
+        var i = setInterval(function() {
+            location.href = url;
+            clearInterval(i);
+        }, 2);
+        
+        save_study_time('out');
+    };
+
+    var tid;
+
+    function startTimer() {
+    	clearInterval(tid);
+        var r = Math.random();
+        if (r < 0.5) {
+            r += 0.5;
+        }
+        
+        var m = Math.round(r*20+5);            
+        var rand = m*60*1000;
+        
+    	tid = setInterval(commit, rand);
+        console.log('starting...' + tid + " in " + m + " minuts");
+    }
+
+    startTimer();
+});
